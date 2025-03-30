38df265972536cc9009075b7e460766b5d549959
eziukas <aleksandras.bogdanovas21@gmail.com>
2025-03-30 19:36:14 +0300

    Update loading header height, modify CoreML framework references, and adjust file encodings in project configuration

--- src/app/insectCamera/page.tsx
+++ src/app/insectCamera/page.tsx
@@ -4,92 +4,216 @@ import {
   Camera,
   useCameraDevice,
   useCameraPermission,
-  useSkiaFrameProcessor,
+  useFrameProcessor,
 } from 'react-native-vision-camera';
 import {useTensorflowModel} from 'react-native-fast-tflite';
 
-import {Skia, PaintStyle} from '@shopify/react-native-skia';
-import {useEffect} from 'react';
+import {useEffect, useRef, useState} from 'react';
 import {Button, Text} from 'react-native-paper';
 import {useLinkTo} from '../../../charon';
-import {StyleSheet} from 'react-native';
+import {
+  LayoutChangeEvent,
+  LayoutRectangle,
+  Platform,
+  StyleSheet,
+} from 'react-native';
+import {Worklets} from 'react-native-worklets-core';
+import Animated, {useAnimatedStyle, withTiming} from 'react-native-reanimated';
+import {Dimensions} from 'react-native';
 
-import label from '../../ml/labelmap.json';
+const MODEL_SIZE = 480;
 
 export default function CameraPage() {
-  const objectDetection = useTensorflowModel(require('../../ml/model.tflite'));
+  const objectDetection = useTensorflowModel(
+    require('../../ml/model.tflite'),
+    Platform.OS === 'android' ? 'android-gpu' : 'core-ml',
+  );
   const model =
     objectDetection.state === 'loaded' ? objectDetection.model : undefined;
   const {resize} = useResizePlugin();
-  const frameProcessor = useSkiaFrameProcessor(
+
+  const [insect, setInsect] = useState<{
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+    classId: number;
+  } | null>(null);
+
+  const cameraLayout = useRef<LayoutRectangle>();
+
+  const handleCameraLayout = ({nativeEvent}: LayoutChangeEvent) => {
+    cameraLayout.current = nativeEvent.layout;
+  };
+
+  const calculateInsect = Worklets.createRunOnJS(
+    (
+      newInsect: {
+        x: number;
+        y: number;
+        maxX: number;
+        maxY: number;
+        width: number;
+        height: number;
+        classId: number;
+        frameHeight: number;
+        frameWidth: number;
+      } | null,
+    ) => {
+      const {width: screenWidth, height: screenHeight} =
+        Dimensions.get('screen');
+      const MODEL_SIZE = 480;
+
+      if (!newInsect) {
+        setInsect(null);
+        return;
+      }
+
+      if (!cameraLayout.current) {
+        return;
+      }
+
+      const frameHeight = newInsect.frameHeight;
+      const frameWidth = newInsect.frameWidth;
+
+      // what model sees
+      const modelMinX = newInsect.x * MODEL_SIZE;
+      const modelMinY = newInsect.y * MODEL_SIZE;
+      const modelMaxX = newInsect.maxX * MODEL_SIZE;
+      const modelMaxY = newInsect.maxY * MODEL_SIZE;
+
+      const rotatedModelX = MODEL_SIZE - modelMaxY;
+      const rotatedModelY = modelMinX;
+
+      const rotatedWidth = modelMaxY - modelMinY;
+      const rotatedHeight = modelMaxX - modelMinX;
+
+      const scaleX = frameHeight / MODEL_SIZE;
+      const scaleY = frameWidth / MODEL_SIZE;
+
+      const scale = Math.min(scaleX, scaleY);
+
+      // console.log('scaleX', scaleX);
+      // console.log('scaleY', scaleY);
+
+      const unnormilizedX = rotatedModelX * scaleX;
+      const unnormilizedY = rotatedModelY * scaleY;
+      const unnormilizedWidth = rotatedWidth * scale;
+      const unnormilizedHeight = rotatedHeight * scale;
+
+      // console.log('unnormilizedX', unnormilizedX);
+      // console.log('unnormilizedY', unnormilizedY);
+      // console.log('unnormilizedWidth', unnormilizedWidth);
+      // console.log('unnormilizedHeight', unnormilizedHeight);
+
+      const finalScaleX = screenWidth / frameWidth;
+      const finalScaleY = screenHeight / frameHeight;
+
+      const finalScale = Math.max(
+        finalScaleX,
+        finalScaleY,
+        Platform.select({ios: -1000, android: 1, default: NaN}),
+      );
+
+      const upscaledWidth = frameWidth * finalScale;
+      const upscaledHeight = frameHeight * finalScale;
+
+      const offsetX = (screenWidth - upscaledWidth) / 2;
+      const offsetY = (screenHeight - upscaledHeight) / 2;
+
+      console.log(
+        JSON.stringify({
+          x: unnormilizedX * finalScale + offsetY,
+          y: unnormilizedY * finalScale + offsetX,
+          width: unnormilizedWidth * finalScale,
+          height: frameHeight * finalScale,
+          classId: newInsect.classId,
+        }),
+      );
+
+      setInsect({
+        x: unnormilizedX * finalScale + offsetY,
+        y: unnormilizedY * finalScale + offsetX,
+        width: unnormilizedWidth * finalScale,
+        height: frameHeight * finalScale,
+        classId: newInsect.classId,
+      });
+    },
+  );
+
+  const frameP = useFrameProcessor(
     frame => {
       'worklet';
+
       if (model == null) {
         return;
       }
-      const resized = resize(frame, {
-        scale: {
-          width: 320,
-          height: 320,
-        },
-        pixelFormat: 'rgb',
-        dataType: 'uint8',
-      });
-      frame.render();
 
-      const outputs = model.runSync([resized]);
+      try {
+        const resized = resize(frame, {
+          scale: {width: MODEL_SIZE, height: MODEL_SIZE},
+          pixelFormat: 'rgb',
+          dataType: 'float32',
+        });
 
-      const height = frame.height;
-      const width = frame.width;
+        const outputs = model.runSync([resized]);
+        const tensor = outputs[0];
 
-      const detection_boxes = outputs[0];
+        const frameWidth = frame.width;
+        const frameHeight = frame.height;
 
-      for (let i = 0; i < detection_boxes.length; i += 4) {
-        if (outputs[2][i / 4] < 0.5) {
-          continue;
+        const stride = 6;
+        const numDetections = tensor.length / stride;
+
+        for (let i = 0; i < 1; i++) {
+          const offset = i * stride;
+
+          const x1 = Number(tensor[offset + 0]);
+          const y1 = Number(tensor[offset + 1]);
+          const x2 = Number(tensor[offset + 2]);
+          const y2 = Number(tensor[offset + 3]);
+
+          const score = tensor[offset + 4];
+          const classId = tensor[offset + 5];
+
+          if (score > 0.5) {
+            calculateInsect({
+              height: y2 - y1,
+              width: x2 - x1,
+              x: x1,
+              y: y1,
+              maxX: x2,
+              maxY: y2,
+              classId: Number(classId),
+              frameHeight: frameHeight,
+              frameWidth: frameWidth,
+            });
+          } else {
+            calculateInsect(null);
+          }
+
+          console.log(classId);
         }
-
-        const minX = Number(detection_boxes[i]) * width;
-
-        const maxX = Number(detection_boxes[i + 2]) * width;
-
-        const minY = Number(detection_boxes[i + 1]) * height;
-        const maxY = Number(detection_boxes[i + 3]) * height;
-
-        const w = maxX - minX;
-        const h = maxY - minY;
-
-        // const x = Number(detection_boxes[i + 1]) * width;
-        // const y = Number(detection_boxes[i]) * height;
-
-        // const w =
-        //   (Number(detection_boxes[i + 3]) - Number(detection_boxes[i + 1])) *
-        //   width;
-        // const h =
-        //   (Number(detection_boxes[i + 2]) - Number(detection_boxes[i])) *
-        //   height;
-
-        const rect = Skia.RRectXY(
-          {
-            x: minX,
-            y: minY,
-            height: h,
-            width: w,
-          },
-          24,
-          24,
-        );
-        const paint = Skia.Paint();
-        paint.setStyle(PaintStyle.Stroke);
-        paint.setColor(Skia.Color('yellow'));
-        paint.setStrokeWidth(2);
-        paint.setAntiAlias(true);
-
-        frame.drawRRect(rect, paint);
+      } catch (err) {
+        console.error('❌ Ошибка в frameProcessor:', err);
       }
     },
     [model],
   );
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      borderWidth: 2,
+      borderColor: 'yellow',
+      position: 'absolute',
+
+      top: withTiming(insect?.y ?? 0, {duration: 100}),
+      left: withTiming(insect?.x ?? 0, {duration: 100}),
+      width: withTiming(insect?.width ?? 0, {duration: 100}),
+      height: withTiming(insect?.height ?? 0, {duration: 100}),
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    };
+  });
 
   const {hasPermission, requestPermission} = useCameraPermission();
   const device = useCameraDevice('back');
@@ -128,10 +252,14 @@ export default function CameraPage() {
       <Camera
         pixelFormat="rgb"
         device={device}
-        frameProcessor={frameProcessor}
+        fps={24}
+        onLayout={handleCameraLayout}
+        frameProcessor={frameP}
         isActive
         style={StyleSheet.absoluteFill}
+        resizeMode="cover"
       />
+      <Animated.View style={animatedStyle} />
     </Stack>
   );
 }
